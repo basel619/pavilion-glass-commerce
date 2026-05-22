0a9a5b9075ed5c616b8da529bafa44d95c1bd5b0
IRAQ SOFT <IRAQ SOFT@DESKTOP-DCH4OMG>
2026-05-22 19:34:22 +0300

    update map location in contact page

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -31,7 +31,7 @@ function ContactPage() {
         </div>
         <div className="glass-strong rounded-2xl overflow-hidden min-h-[300px]">
           <iframe title="map" className="w-full h-full min-h-[300px]"
-            src="https://www.google.com/maps?q=University+of+Technology+Baghdad&output=embed" />
+            src="https://www.google.com/maps?q=8C5W%2BPGH+%D8%A8%D8%A7%D9%81%D9%84%D9%8A%D9%88%D9%86+%D9%84%D8%AE%D8%AF%D9%85%D8%A7%D8%AA+%D8%A7%D9%84%D8%B5%D9%8A%D8%A7%D9%86%D8%A9+%D9%87%D8%A7%D8%B1%D8%AF+%D8%AF%D9%8A%D8%B3%D9%83+%D9%88%D9%84%D8%A7%D8%A8+%D8%AA%D9%88%D8%A8,+Baghdad&output=embed" />
         </div>
       </div>
     </Layout>
